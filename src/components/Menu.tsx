797f64be949549df609f49e3f034647599cbeb64
cairo castro <cairoantonio9988@hotmail.com>
2026-07-24 12:53:41 -0300

    clean code: criada uma variavel para as classes css dos links dos icones

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,21 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
+const classesLinkIcon =
+    'inline-flex items-center justify-center rounded-xl bg-(--primary) p-5 text-(--text-over-primary) transition-all delay-[0.1s] ease-in-out hover:brightness-75';
+
 export function Menu() {
     return (
         <nav className='flex items-center justify-center gap-6'>
-            <a
-                className='inline-flex items-center justify-center rounded-xl bg-(--primary) p-5
-                    text-(--text-over-primary) transition-all delay-[0,1s] ease-in-out hover:brightness-75'
-                href='#'
-            >
+            <a className={classesLinkIcon} href='#'>
                 <HouseIcon className='h-10 w-10' />
             </a>
-            <a
-                className='inline-flex items-center justify-center rounded-xl bg-(--primary) p-5
-                    text-(--text-over-primary) transition-all delay-[0,1s] ease-in-out hover:brightness-75'
-                href='#'
-            >
+            <a className={classesLinkIcon} href='#'>
                 <HistoryIcon className='h-10 w-10' />
             </a>
-            <a
-                className='inline-flex items-center justify-center rounded-xl bg-(--primary) p-5
-                    text-(--text-over-primary) transition-all delay-[0,1s] ease-in-out hover:brightness-75'
-                href='#'
-            >
+            <a className={classesLinkIcon} href='#'>
                 <SettingsIcon className='h-10 w-10' />
             </a>
-            <a
-                className='inline-flex items-center justify-center rounded-xl bg-(--primary) p-5
-                    text-(--text-over-primary) transition-all delay-[0,1s] ease-in-out hover:brightness-75'
-                href='#'
-            >
+            <a className={classesLinkIcon} href='#'>
                 <SunIcon className='h-10 w-10' />
             </a>
         </nav>
