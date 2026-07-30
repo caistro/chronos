24f53fa8c4a024107505bba7145c0dca7810d86f
cairo castro <cairoantonio9988@hotmail.com>
2026-07-30 16:22:35 -0300

    Implementação de contexto para repasse de estado evitando assim o prop drilling

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,3 +1,10 @@
+import { useTaskContext } from '../contexts/TaskContext/useTaskContext';
+
 export function CountDown() {
-    return <div className='text-center text-[clamp(8rem,30vw,16rem)] leading-tight font-bold'>00:00</div>;
+    const { state } = useTaskContext();
+    return (
+        <div className='text-center text-[clamp(8rem,30vw,16rem)] leading-tight font-bold'>
+            {state.formattedsecondRemaing}
+        </div>
+    );
 }
