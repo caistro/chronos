a1c8fee72f12c2c486c78512e4590759c6371d93
cairo castro <cairoantonio9988@hotmail.com>
2026-07-31 12:56:48 -0300

    Implemtação da captura não controlada do input de task

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -2,12 +2,18 @@ import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Cycles } from './Cycles';
 import { Defaultbutton } from './DefaultButton';
 import { DefaultInput } from './DefaultInput';
+import { useRef, type SubmitEvent } from 'react';
 
 export function MainForm() {
+    const taskNameInput = useRef<HTMLInputElement>(null);
+
+    function handleCreateNewTask(event: SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
     return (
-        <form className='flex flex-col items-center justify-center gap-9'>
+        <form onSubmit={handleCreateNewTask} className='flex flex-col items-center justify-center gap-9'>
             <div className='flex flex-col items-center justify-center gap-9'>
-                <DefaultInput labelText='task' id='input' type='text' placeholder='Digite algo' />
+                <DefaultInput labelText='task' id='input' type='text' placeholder='Digite algo' ref={taskNameInput} />
             </div>
             <div className='flex flex-col items-center justify-center gap-9'>
                 <p>Lorem ipsum dolor sit amet.</p>
